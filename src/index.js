cf17de496d778b70e231968e97db05e6d894b1f0
Dung_Bap <ngovandung397@gmail.com>
2023-04-09 21:45:29 +0700

    change font, create globalStyles

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import GlobalStyles from "./components/GlobalStyles";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <GlobalStyles>
+    <App />
+  </GlobalStyles>
 );
 
 // If you want to start measuring performance in your app, pass a function
